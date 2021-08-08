43a3014a69ab7c2f83999012d39f8edc6b1b3b3f
dudesahn <dudesahn@gmail.com>
2021-08-08 15:27:11 -0400

    chore: linting

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -21,15 +21,17 @@ def test_operation(chain, accounts, token, zap, whale, newToken):
     print("Ending v1 triCrypto vault token balance of whale:", final_old_balance / 1e18)
     print("Ending v2 triCrypto vault token balance of whale:", final_new_balance / 1e18)
 
-	# Make sure we don't have any old yToken left and have a positive balance of the new one
+    # Make sure we don't have any old yToken left and have a positive balance of the new one
     assert final_new_balance > 0
     assert final_old_balance == 0
 
-	# Make sure we have about the same amount of underlying within 1%
+    # Make sure we have about the same amount of underlying within 1%
     underlying_original = token.pricePerShare() * starting_old_balance / 1e18
     underlying_new = newToken.pricePerShare() * final_new_balance / 1e18
-    
-    print("Underlying balance of v1 vault token:", underlying_original/1e18)
-    print("Underlying balance of v2 vault token:", underlying_new/1e18)
 
-    assert math.isclose(underlying_new, underlying_original, abs_tol=underlying_original * 0.01)
+    print("Underlying balance of v1 vault token:", underlying_original / 1e18)
+    print("Underlying balance of v2 vault token:", underlying_new / 1e18)
+
+    assert math.isclose(
+        underlying_new, underlying_original, abs_tol=underlying_original * 0.01
+    )
